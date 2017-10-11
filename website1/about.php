a1c7398c452e2c8991f79c2f65c6106bb18febd9
fowerking.mywork.com <845457928@qq.com>
2017-10-11 17:42:22 +0800

    add

---
+++ website1/about.php
@@ -0,0 +1,16 @@
+<?php include_once "./inc/header.php" ?>
+<?php include_once "./inc/header.php" ?>
+	<h1>About</h1>
+<?php include "./inc/footer.php" ?>
+
+<!-- 
+	include & require 的区别
+
+	如果有报错信息，include会显示报错信息，但不影响后面代码执行
+
+	如果有报错信息，require会显示报错信息，但后面的代码不会执行
+
+	include_once & require_once 的作用
+	
+	重复引入，只识别一次！！！
+ -->
